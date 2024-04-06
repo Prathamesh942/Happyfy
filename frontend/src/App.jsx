20cc848f23ea874b07c33fd03dc408f8eccfba09
prathamesh <prathameshsk94@gmail.com>
2024-04-06 17:00:13 +0530

    setup react project with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className=' bg-red-100'>Happyfy</div>
+     <div className=' text-blue-600'>hii</div>
+    </>
+  )
+}
+
+export default App
